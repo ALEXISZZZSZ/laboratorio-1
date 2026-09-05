03852d448118e9631caf4941aa6af6dc6d099648
yasslun <yaseidyhuamanluna@gmail.com>
2026-09-05 10:57:41 -0500

    Replace initial greetings with 'hola mundo upn'

--- main.py
+++ main.py
@@ -1,4 +1,1 @@
-# programa principal del proyecto colaborativo 
-print ("PROYECTO COLABORATIVO - FUNDAMENTOS")
-print ("hola Saludo inicial del programa.")
-print ("aporte personal agregado exitosamente al proyecto")
+print("hola mundo upn")
