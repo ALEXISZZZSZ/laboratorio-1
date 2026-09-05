ea5b7a30fcf8bd8afb0ca8dba99b612d0d052d79
yasslun <yaseidyhuamanluna@gmail.com>
2026-09-05 10:52:27 -0500

    Add main program for collaborative project

---
+++ main.py
@@ -0,0 +1,4 @@
+# programa principal del proyecto colaborativo 
+print ("PROYECTO COLABORATIVO - FUNDAMENTOS")
+print ("hola Saludo inicial del programa.")
+print ("aporte personal agregado exitosamente al proyecto")
